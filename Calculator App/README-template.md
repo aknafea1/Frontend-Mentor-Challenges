cf5839b5faa811c9205a4fb3d46218b2d6a8eca4
Ahmed Kamal <nafeaworks@gmail.com>
2025-12-27 02:32:58 +0200

    added Calculator App Challenge

--- Calculator App/README-template.md
+++ Calculator App/README-template.md
@@ -1,6 +1,6 @@
-# Frontend Mentor - Blog preview card solution
+# Frontend Mentor - Calculator app solution
 
-This is a solution to the [Blog preview card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
@@ -18,7 +18,9 @@ This is a solution to the [Blog preview card challenge on Frontend Mentor](https
 
 ### Screenshot
 
-![](./screenshot.png)
+![](./screenshot1.png)
+![](./screenshot2.png)
+![](./screenshot3.png)
 
 ### Links
 
@@ -32,8 +34,10 @@ This is a solution to the [Blog preview card challenge on Frontend Mentor](https
 - Semantic HTML5 markup
 - CSS custom properties
 - CSS Flex
+- Css Grid
 - Mobile-first workflow
-
+- Vanilla js
+- Regex
 
 ## Author
 
---
+++ Calculator App/script/main.js
@@ -0,0 +1,52 @@
+const screen = document.querySelector(".screen");
+const themeContainer = document.getElementById("switch");
+const themeButtons = document.querySelectorAll("#switch li");
+const keybadButtons = document.querySelectorAll(".buttons div");
+const numReg = /\B(?=(\d{3})+(?!\d))/g;
+const numbersFormat = /[0-9\.\+\\\*-]+/;
+function numberForamtter() {
+  screen.textContent = screen.textContent.replace(numReg, ",");
+}
+
+themeButtons.forEach((button, idx) => {
+  button.addEventListener("click", function () {
+    document.body.className = `theme-${idx + 1}`;
+    themeContainer.style.setProperty("--switch-position", `${idx * 33}%`);
+  });
+});
+function handleKeyPress(keyValue) {
+  switch (keyValue) {
+    case "del":
+      screen.textContent = screen.textContent.slice(0, -1);
+      break;
+    case "reset":
+      screen.textContent = "";
+      break;
+    case "x":
+      screen.textContent = screen.textContent + "*";
+      break;
+    case "=":
+      if (screen.textContent.match(numbersFormat)) {
+        screen.textContent = eval(screen.textContent);
+      }
+      break;
+
+    default:
+      screen.textContent += keyValue;
+  }
+}
+keybadButtons.forEach(function (key) {
+  key.addEventListener("click", function () {
+    screen.textContent = screen.textContent.split(",").join("");
+    handleKeyPress(this.textContent);
+    numberForamtter();
+  });
+  key.addEventListener("mousedown", function () {
+    this.style.setProperty("--box-shadow-depth", "0px");
+    this.style.setProperty("transform", "translateY(2px)");
+  });
+  key.addEventListener("mouseup", function () {
+    this.style.setProperty("--box-shadow-depth", "2px");
+    this.style.setProperty("transform", "translateY(0px)");
+  });
+});
